c76fe6cc8746cbe7e176814b6d120ab581115d13
MSI75 <sisaif56@gmail.com>
2023-11-27 08:23:32 +0600

    print

---
+++ PythonInterveiw/print.py
@@ -0,0 +1,49 @@
+
+# print 
+# print(object= separator= end= file= flush=)
+
+"""
+01.tow line print two print()
+print('A') 
+print('B')
+"""
+
+"""
+# 02.one line print with two print()
+print('A', end='')
+print('B')
+"""
+
+
+"""
+03.with sep parameter
+print('Bangladesh', 'Dhaka', 'Barisal', sep='')
+"""
+
+"""
+# 04. print variable and literals
+number = -10.6
+name = 'Bangladesh'
+
+#literals
+print(5)
+# variable
+print(number)
+print(name)
+"""
+
+"""
+# 05. print concatenated string
+print('Bangladesh is '+'beautiful.')
+"""
+
+"""
+# 6.output formating
+x = 5
+y = 7
+print('The value of x is {} and y is {}'.format(x,y))
+print('The value of x is', x, end=" ")
+print('and y is', y)
+"""
+
+
