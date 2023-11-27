6188df69a591d9a5e000887f1742bdad8bffe39f
MSI75 <sisaif56@gmail.com>
2023-11-27 13:07:50 +0600

    loop

---
+++ PythonInterveiw/loop.py
@@ -0,0 +1,36 @@
+
+# for i in range(1, 101):
+#     print(i , end=" ")
+
+# for i in range(1, 101):
+#     if i%2==0:
+#         print(i, end=" ")
+
+# sum = 0
+# for i in range(1, 101):
+#     sum += i
+# print(sum)
+
+# i = 1
+# while 1:
+#     print(i, end=" ")
+#     i+=1
+#     if i==101:break
+
+# prime number 
+# n = int(input())
+# f = True
+# for i in range(2, n):
+#     if n%i==0:
+#         f = False
+#         break
+# if(f):
+#     print("{} is prime number".format(n))
+# else :
+#     print("{} is not prime number".format(n))
+
+# n = int(input())
+# ans = 1
+# for i in range(1, n+1):
+#     ans*=i
+# print(ans)
