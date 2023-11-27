494be81643dacd7d44eee46ef4b7f12aad43b731
MSI75 <sisaif56@gmail.com>
2023-11-27 09:04:14 +0600

    numeric

---
+++ PythonInterveiw/numeric.py
@@ -0,0 +1,30 @@
+"""
+# 01.addition, subtraction , mulitplication , division -> +, -, *, /
+a = 5
+b = 6
+
+print(b+a)
+print(b-a)
+print(b*a)
+print(b/a)
+"""
+
+"""
+# 02.using the floor division -> // 
+a, b = 3, 2
+print(a//b)
+"""
+
+"""
+# 03.Modulus operator -> %
+a , b = 7, 5
+print(a%b)
+"""
+
+"""
+# 04.Exponentiation operation -> **
+a, b = 2, 3
+print(a**b)
+"""
+
+
