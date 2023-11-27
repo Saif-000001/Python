1e60926d71ab77d936e130c8f9f5a2b1bf14dfa1
MSI75 <sisaif56@gmail.com>
2023-11-27 11:27:13 +0600

    variable

---
+++ PythonInterveiw/variable.py
@@ -0,0 +1,53 @@
+
+"""
+# 01.integer type variable
+a = 12345
+print(a)
+
+# 02.floating type variabl
+b = 14.56
+print(b)
+
+# 03.string type variable 
+s = "MSI"
+print(s)
+"""
+
+"""
+# 04.assign single value to mulitple variables 
+a = b = c = d = 12345
+# print(a, b, c, d)
+print(a)
+print(b)
+print(c)
+print(d)
+"""
+
+"""
+# 05.assign multiple object to multiple variable 
+a, b, c = 123, 45.9, 'MSI'
+# print(a, b, c)
+print(a)
+print(b)
+print(c)
+"""
+
+"""
+# 06. local variable, global variable
+g_val = "Global variabl"
+
+def f():
+    l_val = "local varibal"
+    # print global variable
+    print(g_val)
+
+    # print local variable
+    print(l_val)
+f()
+# print global variable
+print(g_val)
+"""
+
+
+
+
