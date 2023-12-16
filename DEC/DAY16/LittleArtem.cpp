92755a09cdeadfe0ae0bf4d5516b4ef92fa22888
MSI75 <sisaif56@gmail.com>
2023-12-16 12:04:28 +0600

    LittleArtem

---
+++ DEC/DAY16/LittleArtem.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<char>> a(n, vector<char>(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if ((i + j) % 2 == 0)
+                a[i][j] = 1;
+        }
+    }
+
+    if ((n*m)%2==0)
+        a[0][1] = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == 0)
+                cout << 'W';
+            else
+                cout << 'B';
+        }
+        cout << "\n";
+    }
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
