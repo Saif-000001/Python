a4657c8181c206cce3543a401576b448e584fee4
MSI75 <sisaif56@gmail.com>
2023-12-17 11:40:53 +0600

    Greatest Convex

---
+++ DEC/Day17/GreatestConvex.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+ll fact(ll n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * fact(n - 1);
+}
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    cout<<n-1<<"\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
